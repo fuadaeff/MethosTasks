1b8520842717b7e8b3715eed55c32058e53a5959
fuadaeff <fabdulla057@gmail.com>
2024-12-12 21:16:57 +0400

    Add files via upload

---
+++ checkPositiveorNegative/checkPositiveorNegative/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace checkPositiveorNegative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static string check(int a)
+            {
+                if (a < 0)
+                {
+                    return "Negative";
+                }
+                else if (a > 0)
+                {
+                    return "Positive";
+                }
+                else
+                {
+                    return "Zero";
+                }
+            }
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(check(number));
+
+        }
+    }
+}
