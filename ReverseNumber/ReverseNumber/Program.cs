5dc3a5aab82d4a4169675ad84117f967f4381d10
fuadaeff <fabdulla057@gmail.com>
2024-12-12 21:15:58 +0400

    Add files via upload

---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int reverse(int number)
+            {
+                int q = 0;
+                int reversedNumber = 0;
+                if (number < 0)
+                {
+                    number = -number;
+                    while (number > 0)
+                    {
+                        q = number % 10;
+                        number = number / 10;
+                        reversedNumber = (reversedNumber * 10) + q;
+                    }
+                    return -reversedNumber;
+                }
+                else
+                {
+                    while (number > 0)
+                    {
+                        q = number % 10;
+                        number = number / 10;
+                        reversedNumber = (reversedNumber * 10) + q;
+                    }
+                    return reversedNumber;
+                }
+            }
+            Console.WriteLine("Enter a number:");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(reverse(input));
+        }
+    }
+}
