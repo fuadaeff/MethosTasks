92562de4b37d697dd0d5efe21f43caa51b6838e7
fuadaeff <fabdulla057@gmail.com>
2024-12-12 21:16:30 +0400

    Add files via upload

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            static int Factorial(int number)
+            {
+                int fact = 1;
+                for (int i = 1; i <= number; i++)
+                {
+                    fact = fact * i;
+                }
+                return fact;
+            }
+            Console.WriteLine("Enter a number:");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(input));
+        }
+    }
+}
